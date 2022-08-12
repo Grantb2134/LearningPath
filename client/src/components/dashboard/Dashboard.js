a589f7d7f83676239830050f5850e010aab04141
Al <dandavisjs@protonmail.com>
2022-08-12 13:07:33 -0700

    Dashboard page

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import styles from './dashboard.module.scss';
 
 function Dashboard() {
   return (
-    <div>
-      dashboard
+    <div className={styles.container}>
+      <h2>Memorization Guide: JS/HTML/CSS</h2>
+      <div>
+        <div>
+					<div>concepts</div>
+        </div>
+        <div className={styles.sidebar}>
+					<div>sidebar</div>
+        </div>
+      </div>
     </div>
   );
 }
